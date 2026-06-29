1106ee6415199b9eb62c9536724825e7225d0a5b
kait-ro <dabruanikait@gmail.com>
2026-06-29 18:48:57 +0530

    added animation

--- lagrangian.py
+++ lagrangian.py
@@ -3,6 +3,9 @@ import scipy as sp
 import matplotlib.pyplot as plt
 from scipy.integrate import solve_ivp
 from scipy.constants import g
+from matplotlib.animation import FuncAnimation
+
+
 
 class Pendulum():
     def __init__(self, length1, length2, mass1, mass2, theta1, theta2, omega1=0.0, omega2=0.0):
@@ -16,13 +19,14 @@ class Pendulum():
         self.omega2 = omega2
         self.initial_state = np.array([self.theta1, self.theta2, self.omega1, self.omega2])
 
-    def coordinates(self):
-        x1 = self.length1*np.sin(self.theta1)
-        y1 = -self.length1*np.cos(self.theta1)
-        x2 = self.length1*np.sin(self.theta1) + self.length2*np.sin(self.theta2)
-        y2 = -(self.length1*np.cos(self.theta1) + self.length2*np.cos(self.theta2))
+    def coordinates(self, theta_one, theta_two):
+        x1 = self.length1*np.sin(theta_one)
+        y1 = -self.length1*np.cos(theta_one)
+        x2 = self.length1*np.sin(theta_one) + self.length2*np.sin(theta_two)
+        y2 = -(self.length1*np.cos(theta_one) + self.length2*np.cos(theta_two))
         coordinates = np.array([[x1,y1], [x2,y2]])
         return coordinates
+
 
     def equations_of_motions(self, t, state):
         theta1, theta2, omega1, omega2 = state #unpacking tuple
@@ -48,14 +52,40 @@ class Pendulum():
                 self.length2*(
                     2*self.mass1+self.mass2-self.mass2*np.cos(2*theta1-2*theta2)
                 ))
-        sol = np.array([omega1, omega2, alpha1, alpha2])
-        return sol
+        result = np.array([omega1, omega2, alpha1, alpha2])
+        return result
     
     def simulate(self, time_span, time_eval):
         sol = solve_ivp(self.equations_of_motions, time_span, self.initial_state, t_eval = time_eval)
         return sol
 
-'''initial_pendulum  = Pendulum(1.0, 1.5, 1.0, 1.0, 15, 30)
-print(initial_pendulum.coordinates())
-teval = np.arange(0,10, 0.5)
-print(initial_pendulum.simulate((0,10), teval))'''
+
+initial_pendulum  = Pendulum(2, 2, 100, 50, 75, 15)
+teval = np.arange(0,400, 0.5)
+
+fig, axis = plt.subplots()
+axis.set_xlim(-4,4)
+axis.set_ylim(-4,4)
+animated_plot, = plt.plot([],[])
+bobs, = plt.plot([], [], marker='o', linestyle='none')
+
+store_state = initial_pendulum.simulate((0,400), teval)
+
+def update(frame):
+    theta1, theta2 = store_state.y[0][frame], store_state.y[1][frame]
+    coordinates_at_frame = initial_pendulum.coordinates(theta1, theta2)
+    y2 = coordinates_at_frame[1,1]
+    y1 = coordinates_at_frame[0,1]
+    x2 = coordinates_at_frame[1,0]
+    x1 = coordinates_at_frame[0,0]
+    animated_plot.set_data([0, x1,x2], [0, y1,y2])
+    bobs.set_data([x1, x2], [y1, y2])
+    pass
+
+animation = FuncAnimation(
+    fig=fig,
+    func=update,
+    frames=len(teval),
+    interval = 66,
+)
+plt.show()
